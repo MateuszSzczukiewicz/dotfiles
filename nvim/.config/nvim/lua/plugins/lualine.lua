0f71cb7a27f56aa0cfa8836fb07cdec0f51c88eb
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-11-03 21:18:54 +0100

    added few things

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,65 +1,65 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "rose-pine/neovim" },
-	config = function()
-		local lualine = require("lualine")
-		local rose_pine = require("rose-pine.palette")
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", "rose-pine/neovim" },
+  config = function()
+    local lualine = require("lualine")
+    local rose_pine = require("rose-pine.palette")
 
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = rose_pine.love, fg = rose_pine.base, gui = "bold" },
-				b = { bg = rose_pine.base, fg = rose_pine.text },
-				c = { bg = rose_pine.surface, fg = rose_pine.text },
-			},
-			insert = {
-				a = { bg = rose_pine.iris, fg = rose_pine.base, gui = "bold" },
-				b = { bg = rose_pine.base, fg = rose_pine.text },
-				c = { bg = rose_pine.surface, fg = rose_pine.text },
-			},
-			visual = {
-				a = { bg = rose_pine.foam, fg = rose_pine.base, gui = "bold" },
-				b = { bg = rose_pine.base, fg = rose_pine.text },
-				c = { bg = rose_pine.surface, fg = rose_pine.text },
-			},
-			command = {
-				a = { bg = rose_pine.gold, fg = rose_pine.base, gui = "bold" },
-				b = { bg = rose_pine.base, fg = rose_pine.text },
-				c = { bg = rose_pine.surface, fg = rose_pine.text },
-			},
-			replace = {
-				a = { bg = rose_pine.love, fg = rose_pine.base, gui = "bold" },
-				b = { bg = rose_pine.base, fg = rose_pine.text },
-				c = { bg = rose_pine.surface, fg = rose_pine.text },
-			},
-			inactive = {
-				a = { bg = rose_pine.overlay, fg = rose_pine.subtle, gui = "bold" },
-				b = { bg = rose_pine.overlay, fg = rose_pine.subtle },
-				c = { bg = rose_pine.overlay, fg = rose_pine.subtle },
-			},
-		}
+    local my_lualine_theme = {
+      normal = {
+        a = { bg = rose_pine.love, fg = rose_pine.base, gui = "bold" },
+        b = { bg = rose_pine.base, fg = rose_pine.text },
+        c = { bg = rose_pine.surface, fg = rose_pine.text },
+      },
+      insert = {
+        a = { bg = rose_pine.iris, fg = rose_pine.base, gui = "bold" },
+        b = { bg = rose_pine.base, fg = rose_pine.text },
+        c = { bg = rose_pine.surface, fg = rose_pine.text },
+      },
+      visual = {
+        a = { bg = rose_pine.foam, fg = rose_pine.base, gui = "bold" },
+        b = { bg = rose_pine.base, fg = rose_pine.text },
+        c = { bg = rose_pine.surface, fg = rose_pine.text },
+      },
+      command = {
+        a = { bg = rose_pine.gold, fg = rose_pine.base, gui = "bold" },
+        b = { bg = rose_pine.base, fg = rose_pine.text },
+        c = { bg = rose_pine.surface, fg = rose_pine.text },
+      },
+      replace = {
+        a = { bg = rose_pine.love, fg = rose_pine.base, gui = "bold" },
+        b = { bg = rose_pine.base, fg = rose_pine.text },
+        c = { bg = rose_pine.surface, fg = rose_pine.text },
+      },
+      inactive = {
+        a = { bg = rose_pine.overlay, fg = rose_pine.subtle, gui = "bold" },
+        b = { bg = rose_pine.overlay, fg = rose_pine.subtle },
+        c = { bg = rose_pine.overlay, fg = rose_pine.subtle },
+      },
+    }
 
-		lualine.setup({
-			options = {
-				theme = my_lualine_theme,
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-		})
-	end,
+    lualine.setup({
+      options = {
+        theme = my_lualine_theme,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
 }
