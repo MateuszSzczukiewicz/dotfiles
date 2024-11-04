1696070249e4a9f2fbe276a9e2603461b265f69a
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-11-04 10:49:24 +0100

    added marks.nvim

---
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -0,0 +1,24 @@
+return {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+  opts = {
+    default_mappings = true,
+    refresh_interval = 250,
+    builtin_marks = { ".", "<", ">", "^" },
+    cyclic = true,
+    signs = true,
+    sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+    bookmark_0 = {
+      sign = "⚑",
+      virt_text = "Marker",
+      virt_text_pos = "eol",
+      virt_text_highlight = "Comment",
+    },
+    mappings = {
+      toggle = "mm",
+      next = "mn",
+      prev = "mp",
+      delete_buf = "md",
+    },
+  },
+}
