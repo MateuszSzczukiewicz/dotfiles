70bdce75d0403389529ff67cd258207319ce42f1
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-11-09 16:43:41 +0100

    added branch name

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -13,8 +13,23 @@ return {
     vim.cmd("colorscheme rose-pine")
 
     local base = "#191724"
+    vim.cmd("highlight statusline guifg=#ffffff guibg=" .. base)
+    vim.cmd("highlight statuslinenc guifg=#7c7c7c guibg=" .. base)
 
-    vim.cmd("highlight StatusLine guifg=#ffffff guibg=" .. base)
-    vim.cmd("highlight StatusLineNC guifg=#7c7c7c guibg=" .. base)
+    -- Funkcja do uzyskania nazwy brancha Git
+    local function get_git_branch()
+      local handle = io.popen("git branch --show-current 2>/dev/null")
+      if handle then
+        local branch = handle:read("*a"):gsub("\n", "")
+        handle:close()
+        return branch
+      end
+      return ""
+    end
+
+    -- Ustawienia statusline
+    vim.o.laststatus = 2 -- Włącza statusline tylko dla aktywnego okna
+    vim.o.statusline = "%#StatusLine# %f %m" -- Nazwa pliku i modyfikacja
+    vim.o.statusline = vim.o.statusline .. " %=" .. " " .. get_git_branch()
   end,
 }
